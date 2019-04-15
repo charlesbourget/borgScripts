5d05c28e7febf1c6b51bee6e50ffee9de6fa6571
charles <charles.bourget.2@ens.etsmtl.ca>
2019-04-15 15:26:21 -0400

    Script to create new archive

---
+++ createArchive.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#Get current date
+date="$(date +'%Y/%m/%d')"
+
+echo "Creating an archive from ~/Documents, ~/Pictures and ~/Videos"
+
+#Check if the archive name is correct
+echo "The archive will be named : $date"
+read -p "Is the name ok?[yN] " answer
+if [ $answer = 'n' ]
+then
+    read -p "Enter name for new archive : " name
+elif [ $answer = 'y' ]
+then
+    name=$date
+else
+    echo "The answer was not y or n"
+    exit 1
+fi
+
+#Check if the share is mounted on /mnt/
+if [ ! -d /mnt/backup ]
+then
+    echo "The backup repo which is supposed to be on /mnt/backup does not exist."
+    exit 1
+fi
+
+#Create the archive
+echo "Creating archive..."
+
+sudo borg create --progress /mnt/backup::$name ~/Documents ~/Pictures ~/Videos
