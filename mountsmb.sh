9c620faaf03ef1ac9b36a1c9aab3c7aebb33ea5e
charles <charles.bourget.2@ens.etsmtl.ca>
2019-04-15 14:37:25 -0400

    Mount and unmount scripts

---
+++ mountsmb.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+read -p "Enter user to mount samba share as : " username
+read -p "Enter samba share address (ex: 192.168.0.119) : " address
+read -p "Enter samba share name (ex: share) : " share
+
+echo "The share will be mounted in /mnt"
+echo "Mounting $address/$share as $username..."
+
+sudo mount -t cifs -o username=$username "//$address/$share" /mnt
+
+echo "$share is mounted!"
---
+++ unmountsmb.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Unmounting /mnt ..."
+
+sudo unmount /mnt
