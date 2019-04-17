8c12ce9fd59a708efe4f735bc407be37c7abd090
charles <charles.bourget.2@ens.etsmtl.ca>
2019-04-16 20:05:05 -0400

    Added option to mount share if it is not mounted

--- createArchive.sh
+++ createArchive.sh
@@ -5,6 +5,34 @@ DATE="$(date +'%Y-%m-%d')"
 
 echo "Creating an archive from ~/Documents, ~/Pictures and ~/Videos"
 
+#Check if the share is mounted on /mnt/
+if [ ! -d /mnt/backup ]
+then
+    echo "The backup repo which is supposed to be on /mnt/backup does not exist."
+    echo "This is caused by the share not being mounted or because the repo doesn't exist"
+    read -p "Do you want to mount a samba share?[yN] " answer
+
+    if [ $answer = 'n' ]
+    then
+        echo "Backup can\'t be executed without a mounted share..."
+        exit 1
+    elif [ $answer = 'y' ]
+    then
+        #Calls script to mount samba share
+        ./mountsmb.sh
+
+        #Check if the operation was successfull
+        if [ ! -d /mnt/backup ]
+        then
+            echo "Share is still not mounter or repo doesn't exist"
+            exit 1
+        fi
+    else
+        echo "The answer was not y or n"
+        exit 1
+    fi
+fi
+
 #Check if the archive name is correct
 echo "The archive will be named : $DATE"
 read -p "Is the name ok?[yN] " answer
@@ -16,13 +44,6 @@ then
     name=$DATE
 else
     echo "The answer was not y or n"
-    exit 1
-fi
-
-#Check if the share is mounted on /mnt/
-if [ ! -d /mnt/backup ]
-then
-    echo "The backup repo which is supposed to be on /mnt/backup does not exist."
     exit 1
 fi
 
