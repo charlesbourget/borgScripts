79d418d3eb99f2f95ed577c649eff635093ec26d
charles <charles.bourget.2@ens.etsmtl.ca>
2019-04-19 17:04:39 -0400

    Updated string formatting

--- createArchive.sh
+++ createArchive.sh
@@ -4,17 +4,18 @@
 DATE="$(date +'%Y-%m-%d')"
 
 echo "Creating an archive from ~/Documents, ~/Pictures and ~/Videos"
+echo
 
 #Check if the share is mounted on /mnt/
 if [ ! -d /mnt/backup ]
 then
-    echo "The backup repo which is supposed to be on /mnt/backup does not exist."
-    echo "This is caused by the share not being mounted or because the repo doesn't exist"
+    echo -e "\e[31mThe backup repo which is supposed to be on /mnt/backup does not exist."
+    echo -e "\e[39mThis is caused by the share not being mounted or because the repo doesn't exist"
     read -p "Do you want to mount a samba share?[yN] " answer
 
     if [[ $answer = 'n' ]]
     then
-        echo "Backup can't be executed without a mounted share..."
+        echo -e "\e[31mBackup can't be executed without a mounted share..."
         exit 1
     elif [[ $answer = 'y' ]]
     then
@@ -24,11 +25,11 @@ then
         #Check if the operation was successfull
         if [ ! -d /mnt/backup ]
         then
-            echo "Share is still not mounter or repo doesn't exist"
+            echo -e "\e[31mShare is still not mounted or repo doesn't exist"
             exit 1
         fi
     else
-        echo "The answer was not y or n"
+        echo -e "\e[31mThe answer was not y or n"
         exit 1
     fi
 fi
@@ -36,6 +37,7 @@ fi
 #Check if the archive name is correct
 echo "The archive will be named : $DATE"
 read -p "Is the name ok?[yN] " answer
+echo
 if [[ $answer = 'n' ]]
 then
     read -p "Enter name for new archive : " name
@@ -43,7 +45,7 @@ elif [[ $answer = 'y' ]]
 then
     name=$DATE
 else
-    echo "The answer was not y or n"
+    echo -e "\e[31mThe answer was not y or n"
     exit 1
 fi
 
@@ -52,7 +54,7 @@ expression=$(sudo borg list /mnt/backup | grep -w $name)
 
 if [[ -n "$expression" ]]
 then
-    echo "An archive with this name already exist"
+    echo -e "\e[31mAn archive with this name already exist"
     exit 1
 else
     echo "Archive name is unused and valid"
@@ -64,7 +66,7 @@ echo "Creating archive..."
 #Get current time in seconds since UNIX EPOCH
 STARTTIME="$(date +%s)"
 
-sudo borg create --progress /mnt/backup::$name ~/Documents ~/Pictures ~/Videos
+#sudo borg create --progress /mnt/backup::$name ~/Documents ~/Pictures ~/Videos
 
 #Get current time in seconds since UNIX EPOCH
 ENDTIME="$(date +%s)"
